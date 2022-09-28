cc01515bb116ed0d858d828d281ee5d57e08c7ad
Davor Roman <davoroman01@gmail.com>
2022-09-28 17:23:56 +0200

    Layout Web site

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { MainComponent } from './layout/main/main.component';
+import { NavbarComponent } from './layout/header/navbar/navbar.component';
+import { HeroImageComponent } from './layout/header/hero-image/hero-image.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent, HeaderComponent, FooterComponent, MainComponent, NavbarComponent, HeroImageComponent],
+	imports: [BrowserModule],
+	providers: [],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
